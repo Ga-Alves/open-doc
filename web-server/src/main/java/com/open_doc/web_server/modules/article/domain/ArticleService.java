1ae09802cbaefecddf6aeb0a6c15c7b5fe6efc8a
Ga-Alves <gabrielalvesreis@gmail.com>
2026-07-30 11:35:30 -0300

    feat: :sparkles: add edit article endpoint

--- web-server/src/main/java/com/open_doc/web_server/modules/article/domain/ArticleService.java
+++ web-server/src/main/java/com/open_doc/web_server/modules/article/domain/ArticleService.java
@@ -1,12 +1,16 @@
 package com.open_doc.web_server.modules.article.domain;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.open_doc.web_server.modules.article.domain.dtos.ArticleResponseDTO;
 import com.open_doc.web_server.modules.article.domain.dtos.CreateArticleRequestDTO;
+import com.open_doc.web_server.modules.article.domain.dtos.UpdateArticleRequestDTO;
 import com.open_doc.web_server.modules.article.repository.ArticleEntity;
 import com.open_doc.web_server.modules.article.repository.ArticleRepository;
 
@@ -28,12 +32,29 @@ public class ArticleService {
 
     public ArticleResponseDTO createArticle(CreateArticleRequestDTO payload) {
 
-        ArticleEntity newArticle = ArticleEntity.builder().title(payload.title()).content(payload.content())
+        ArticleEntity newArticle = ArticleEntity.builder()
+                .title(payload.title())
+                .content(payload.content())
                 .build();
 
         ArticleEntity savedArticle = articleRepository.save(newArticle);
 
         return new ArticleResponseDTO(savedArticle);
+    }
+
+    public ArticleResponseDTO updateArticle(UpdateArticleRequestDTO payload, UUID articleId) {
+
+        ArticleEntity article = articleRepository.findById(articleId).orElseThrow(
+                () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Article not found with id: " + articleId));
+
+        article.setTitle(payload.title());
+        article.setContent(payload.content());
+
+        articleRepository.save(article);
+
+        return new ArticleResponseDTO(article);
     }
 
 }
---
+++ web-server/src/main/java/com/open_doc/web_server/modules/article/domain/dtos/UpdateArticleRequestDTO.java
@@ -0,0 +1,5 @@
+package com.open_doc.web_server.modules.article.domain.dtos;
+
+public record UpdateArticleRequestDTO(String title, String content) {
+
+}
