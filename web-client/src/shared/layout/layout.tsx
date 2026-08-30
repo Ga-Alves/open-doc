ac8806773826cbe95740e17ef81a11a5b5e9dd6a
Ga-Alves <gabrielalvesreis@gmail.com>
2026-08-30 13:10:01 -0300

    feat: :lipstick: add footer

--- web-client/src/shared/layout/layout.tsx
+++ web-client/src/shared/layout/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import Header from "./header";
+import Footer from "./footer";
 
 type LayoutProps = {
   children: ReactNode;
@@ -7,9 +8,12 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-50/50 font-sans text-gray-900 antialiased">
-      <Header />
-      <main className="max-w-4xl mx-auto px-6 py-10">{children}</main>
+    <div className="min-h-screen flex flex-col justify-between bg-gray-50/50 font-sans text-gray-900 antialiased">
+      <div>
+        <Header />
+        <main className="max-w-4xl mx-auto px-6 py-10">{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
